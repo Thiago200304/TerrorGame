080d9339a0f7f922ced794104884e0dc33feaad0
beduart00 <beatriz84268@gmail.com>
2024-09-23 11:06:34 -0300

    base do menu

---
+++ Assets/Script/SairDoJogo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SairDoJogo : MonoBehaviour
+{
+
+    public void ExitGame()
+    {
+
+        Application.Quit();
+
+    }
+}
