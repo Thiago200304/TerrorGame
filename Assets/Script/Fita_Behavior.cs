4f24c95da6ed1dde3b4325584ac2107fe333f800
LucasBazan16 <lucasbazan069@gmail.com>
2024-11-07 17:03:06 -0300

    menuTextos

---
+++ Assets/Script/Fita_Behavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fita_Behavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+//A sala, coração da casa, esconde mais do que parece. Atrás de um quadro antigo, por trás de um móvel pesado, ou até mesmo dentro de um livro volumoso na estante, uma fita cassete aguarda ser encontrada. O tique-taque do relógio de parede ecoa como um lembrete do tempo que passa e dos segredos que se perdem
